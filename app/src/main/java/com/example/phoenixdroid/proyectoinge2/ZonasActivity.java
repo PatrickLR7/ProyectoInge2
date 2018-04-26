f7f738a635ea84f7457fae1ba63f42adfef15b33
carlos <carlosmellado8194@hotmail.com>
2018-04-26 12:36:57 -0600

    activity videos

--- app/src/main/java/com/example/phoenixdroid/proyectoinge2/ZonasActivity.java
+++ app/src/main/java/com/example/phoenixdroid/proyectoinge2/ZonasActivity.java
@@ -10,4 +10,18 @@ public class ZonasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zonas);
     }
+
+    /*
+    <LinearLayout
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical">
+
+        <ImageView
+    android:id="@+id/imageView5"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    app:srcCompat="@+drawable/vector" />
+    </LinearLayout>
+    */
 }
