59c18c98a638b1695c24a2dbb14d91d520adf02a
PatrickLR7 <patrickrx7@gmail.com>
2018-04-26 19:36:22 -0600

    Zonas asignadas correctamente a botones de activity_zonas.

--- app/src/main/java/com/example/phoenixdroid/proyectoinge2/ZonasActivity.java
+++ app/src/main/java/com/example/phoenixdroid/proyectoinge2/ZonasActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.phoenixdroid.proyectoinge2.Utils.Config;
+import com.example.phoenixdroid.proyectoinge2.Utils.Zona;
+
 public class ZonasActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +18,63 @@ public class ZonasActivity extends AppCompatActivity {
 
     public void actVideos (View v){
         Intent i = new Intent(getApplicationContext(), VideosActivity.class);
-        startActivity(i);
+        switch(v.getId())
+        {
+            case R.id.btnAlvarez:
+                Config.zona = Zona.ENTRADA_CALLE_LOS_ALVAREZ;
+                startActivity(i);
+                break;
+            case R.id.btnCañada:
+                Config.zona = Zona.CALLE_LA_CANADA;
+                startActivity(i);
+                break;
+            case R.id.btnCDelgado:
+                Config.zona = Zona.CALLE_LOS_DELGADO;
+                startActivity(i);
+                break;
+            case R.id.btnCPRioUruca:
+                Config.zona = Zona.CALLE_PARALELA_RIO_URUCA;
+                startActivity(i);
+                break;
+            case R.id.btnCruceP:
+                Config.zona = Zona.CRUCE_PABELLON;
+                startActivity(i);
+                break;
+            case R.id.btnLaFuente:
+                Config.zona = Zona.PUENTE_SECTOR_LA_FUENTE;
+                startActivity(i);
+                break;
+            case R.id.btnMatinilla:
+                Config.zona = Zona.MATINILLA;
+                startActivity(i);
+                break;
+            case R.id.btnPMachete:
+                Config.zona = Zona.PASO_MACHETE;
+                startActivity(i);
+                break;
+            case R.id.btnQCanoas:
+                Config.zona = Zona.QUEBRADA_CANOAS;
+                startActivity(i);
+                break;
+            case R.id.btnQNavajas:
+                Config.zona = Zona.QUEBRADA_NAVAJAS;
+                startActivity(i);
+                break;
+            case R.id.btnQTapezco:
+                Config.zona = Zona.QUEBRADA_TAPEZCO;
+                startActivity(i);
+                break;
+            case R.id.btnSPitier:
+                Config.zona = Zona.SALIDA_QUEBRADA_PITIER;
+                startActivity(i);
+                break;
+            case R.id.btnSanguijuela:
+                Config.zona = Zona.QUEBRADA_SANGIJUELA;
+                startActivity(i);
+                break;
+            default:
+                throw new RuntimeException("Id de boton desconocido.");
+        }
     }
 
 
