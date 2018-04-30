3c01940ffecc745675bcfdae2fe2f8888d6702d3
PatrickLR7 <patrickrx7@gmail.com>
2018-04-29 18:16:01 -0600

    Archivo puntos_encuentro.xml con nombre, latitud y longitud de los puntos de encuentro. Clase PuntoEncuentro para representar info del XML. En la clase MapActivity se utiliza XMLPullParser para obtener la info y se agregan markers en los puntos.

--- app/src/main/java/com/example/phoenixdroid/proyectoinge2/MapActivity.java
+++ app/src/main/java/com/example/phoenixdroid/proyectoinge2/MapActivity.java
@@ -8,12 +8,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.example.phoenixdroid.proyectoinge2.Utils.PuntoEncuentro;
+
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity implements LocationListener {
 
@@ -23,6 +31,7 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
     GeoPoint routeCenter = new GeoPoint(9.91163,-84.1783);
     GeoPoint routeCenter2 = new GeoPoint(9.910699,-84.176321);
     LocationManager locationmanager;
+    ArrayList<PuntoEncuentro> puntosE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +55,8 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
         addMarker(routeCenter, routeCenter2);
         addMarker(routeCenter2,routeCenter);
 
-
+        parseXML();
+        markersPuntosE();
         locationmanager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
 
@@ -85,12 +95,13 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
 
 
 
-    public void addMarker(GeoPoint Center) {
+    public void addMarker(GeoPoint Center, String nombre) {
 
 
 
         Marker marker= new Marker(mapView);
         marker.setPosition(Center);
+        marker.setTitle(nombre);
 
         //marker.setIcon(getDrawable(R.drawable.loc));
         // marker.setAnchor(1000,1000);
@@ -108,8 +119,63 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
 
     }
 
+    private void parseXML(){
+        XmlPullParserFactory parserFactory;
+        try{
+            parserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserFactory.newPullParser();
+            InputStream is = getAssets().open("puntos_encuentro.xml");
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(is, null);
 
+            processParsing(parser);
+        } catch (XmlPullParserException e){
 
+        } catch (IOException e){
+
+        }
+    }
+
+    public void processParsing(XmlPullParser parser) throws IOException, XmlPullParserException{
+        puntosE = new ArrayList<>();
+        int eventType = parser.getEventType();
+        PuntoEncuentro puntoEActual = null;
+
+        while (eventType != XmlPullParser.END_DOCUMENT){
+            String tag = null;
+
+            switch(eventType){
+                case XmlPullParser.START_TAG:
+                    tag = parser.getName();
+
+                    if("node".equals(tag)){
+                        puntoEActual = new PuntoEncuentro();
+                        puntosE.add(puntoEActual);
+                    } else if (puntoEActual != null){
+                        if("nombre".equals(tag)){
+                            puntoEActual.nombre = parser.nextText();
+                        } else if("lat".equals(tag)){
+                            puntoEActual.latitud = Double.parseDouble(parser.nextText());
+                        } else if ("lon".equals(tag)){
+                            puntoEActual.longitud = Double.parseDouble(parser.nextText());
+                        }
+                    }
+                    break;
+            }
+            eventType = parser.next();
+        }
+
+    }
+
+    public void markersPuntosE(){
+        if(!puntosE.isEmpty()) {
+            for (int i = 0; i < puntosE.size(); i++) {
+                routeCenter.setLatitude(puntosE.get(i).latitud);
+                routeCenter.setLongitude(puntosE.get(i).longitud);
+                addMarker(routeCenter, puntosE.get(i).nombre);
+            }
+        }
+    }
 
 
     @Override
@@ -117,7 +183,7 @@ public class MapActivity extends AppCompatActivity implements LocationListener {
         GeoPoint center = new GeoPoint(location.getLatitude(),location.getLongitude());
 
         mapViewController.animateTo(center);
-        addMarker(center);
+        addMarker(center, "Mi ubicacion");
 
     }
 
---
+++ app/src/main/java/com/example/phoenixdroid/proyectoinge2/Utils/PuntoEncuentro.java
@@ -0,0 +1,10 @@
+package com.example.phoenixdroid.proyectoinge2.Utils;
+
+/**
+ * Created by Patrick on 29/4/2018.
+ */
+
+public class PuntoEncuentro {
+    public String nombre;
+    public double latitud, longitud;
+}
