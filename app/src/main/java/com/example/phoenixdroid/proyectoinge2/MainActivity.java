ced0667ef774bb9d19dddad2d01cc3a7830eb8f3
Patrick <patrickrx7@gmail.com>
2018-05-02 12:23:54 -0600

    Cambio de icono. Arreglos menores.

--- app/src/main/java/com/example/phoenixdroid/proyectoinge2/MainActivity.java
+++ app/src/main/java/com/example/phoenixdroid/proyectoinge2/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.phoenixdroid.proyectoinge2.Utils.Config;
 
@@ -17,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Actualmente no disponible." ,Toast.LENGTH_LONG).show();
+            }
+        });
 
         askPermission();
     }
