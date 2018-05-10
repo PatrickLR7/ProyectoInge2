95f3dc505bd1c1a7ce3b5a8eb0c20b5ba0460217
carlos8194 <carlosmellado8194@hotmail.com>
2018-05-10 16:48:31 -0600

    Base da datos ampliada

--- app/src/main/java/com/example/phoenixdroid/proyectoinge2/Utils/BaseDeDatos.java
+++ app/src/main/java/com/example/phoenixdroid/proyectoinge2/Utils/BaseDeDatos.java
@@ -8,20 +8,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class BaseDeDatos extends SQLiteOpenHelper {
 
     /** Nombres de columnas de las tablas. Representan el esquema de la base de datos.*/
-    private static final String BASE_DE_DATOS = "Santa_Ana";
+    private static final String BASE_DE_DATOS = "Evac_Santa_Ana";
 
     // Esquema tabla Puntos
     private static final String TABLA_1 = "Puntos";
     private static final String PUNTO_ID = "PuntoID";
-    private static final String DESCRIPION = "Descripcion";
     private static final String LATITUD = "Latitud";
     private static final String LONGITUD = "Longitud";
     private static final String RUTA_PERTENECE = "RutaFK";
 
-    // Esquema tabla Rutas
+    // Esquema tabla Ruta
     private static final String TABLA_2 = "Rutas";
     private static final String RUTA_ID = "RutaID";
-    private static final String NOMBRE = "Nombre";
+
+    // Esquema tabla de Puntos seguros, incluye atributos de latitud y longitud
+    private static final String TABLA_3 = "Puntos_Seguros";
+    private static final String DESCRIPION = "Descripcion";
+
+    // Esquema tabla para las señales verticales
+    private static final String TABLA_4 = "Señales_Verticales";
+    private static final String NUMERO_SE = "NumeroDeSeñal";
+    private static final String LADO_SE = "LadoDeSeñal";
 
     /**
      * Clase que maneja la conexion con la base de datos.
@@ -36,13 +43,18 @@ public class BaseDeDatos extends SQLiteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String tablaPuntos = "CREATE TABLE " + TABLA_1 + " (" + PUNTO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                DESCRIPION + " TEXT, " + LATITUD + " REAL, " + LONGITUD + " REAL, " + RUTA_PERTENECE + " INTEGER, "+
+        String tablaPuntos = "CREATE TABLE " + TABLA_1 + " (" + PUNTO_ID + " INTEGER PRIMARY KEY, " +
+                LATITUD + " REAL, " + LONGITUD + " REAL, " + RUTA_PERTENECE + " INTEGER, "+
                 "FOREIGN KEY(" + RUTA_PERTENECE + ") REFERENCES " + TABLA_2 + "(" + RUTA_ID + "))";
-        String tablaRutas = "CREATE TABLE " + TABLA_2 + " (" + RUTA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                NOMBRE + " TEXT)";
+        String tablaRutas = "CREATE TABLE " + TABLA_2 + " (" + RUTA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT)";
+        String tabla_Puntos_Seguros = "CREATE TABLE " + TABLA_3 + " (" + PUNTO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                LATITUD + " REAL, " + LONGITUD + " REAL, " +  DESCRIPION + " TEXT)";
+        String tabla_Se = "CREATE TABLE " + TABLA_4 + " (" + NUMERO_SE + " INTEGER PRIMARY KEY, " +
+                LATITUD + " REAL, " + LONGITUD + " REAL, " + LADO_SE + "TEXT)";
         sqLiteDatabase.execSQL(tablaPuntos);
         sqLiteDatabase.execSQL(tablaRutas);
+        sqLiteDatabase.execSQL(tabla_Puntos_Seguros);
+        sqLiteDatabase.execSQL(tabla_Se);
     }
 
     /**
@@ -60,20 +72,35 @@ public class BaseDeDatos extends SQLiteOpenHelper {
 
     /**
      * Metodo usado para agregar un punto a la base de datos.
-     * @param desc: Una descripcion o el propio nombre del lugar.
      * @param latitud: Coordenada latitud.
      * @param longitud: Coordenada longitud.
-     * @param rutaID: ID de la ruta a la que pertenece o NULL si no pertenece a ninguna
+     * @param id: Identificador del punto.
      * @return true si se agrega el punto exitosamente.
      */
-    public boolean agregarPunto(String desc, double latitud, double longitud, Integer rutaID){
+    public boolean agregarPunto(double latitud, double longitud, int id){
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(DESCRIPION, desc);
         contentValues.put(LATITUD, latitud);
         contentValues.put(LONGITUD, longitud);
-        contentValues.put(RUTA_PERTENECE, rutaID);
+        contentValues.put(PUNTO_ID, id);
         long result = database.insert(TABLA_1, null, contentValues);
+        return result != -1;
+    }
+
+    /**
+     * Metodo usado para agregar puntos seguros a la base de datos.
+     * @param latitud: Coordenada latitud.
+     * @param longitud: Coordenada longitud.
+     * @param descripcion: Descripcion o nombre del punto.
+     * @return true si el punto se agrega exitosamente.
+     */
+    public boolean agregarPuntoSeguro(double latitud, double longitud, String descripcion){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(LATITUD, latitud);
+        contentValues.put(LONGITUD, longitud);
+        contentValues.put(DESCRIPION, descripcion);
+        long result = database.insert(TABLA_3, null, contentValues);
         return result != -1;
     }
 
@@ -85,10 +112,38 @@ public class BaseDeDatos extends SQLiteOpenHelper {
     public boolean agregarRuta(String nombre){
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(NOMBRE, nombre);
         long result = database.insert(TABLA_2, null, contentValues);
         return result != -1;
     }
 
+    /**
+     * Metodo usado para agregar señales verticales.
+     * @param num: Numero de la señal, es un valor unico.
+     * @param lado: Orientacion de la señal.
+     * @param latitud: Coordenada latitud.
+     * @param longitud: Coordenada longitud.
+     * @return true si la señal se agrega exitosamente
+     */
+    public boolean agregarSeñalVertical(int num, String lado, double latitud, double longitud){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(LATITUD, latitud);
+        contentValues.put(LONGITUD, longitud);
+        contentValues.put(NUMERO_SE, num);
+        contentValues.put(LADO_SE, lado);
+        long result = database.insert(TABLA_4, null, contentValues);
+        return result != -1;
+    }
 
+    /**
+     * Metodo para asignarle a un punto dado la ruta a la que pertenece.
+     * @param Ruta_ID: Identificador de la ruta.
+     * @param Punto_ID: Identificador del punto.
+     */
+    public void ajustarPunto_Ruta(int Ruta_ID, int Punto_ID){
+        SQLiteDatabase database = this.getWritableDatabase();
+        String query = "UPDATE " + TABLA_1 + " SET " + RUTA_PERTENECE + " = " +  Ruta_ID + " WHERE " +
+                PUNTO_ID + " = " + Punto_ID;
+        database.execSQL(query);
+    }
 }
